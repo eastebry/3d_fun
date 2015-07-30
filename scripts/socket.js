d107389ee5073e59ffd3f03757cf8526881dad24
Andre Haas <andre@andrehaas.com>
2015-07-30 13:53:03 -0700

    Reindent

--- scripts/socket.js
+++ scripts/socket.js
@@ -3,27 +3,28 @@ CLIENT_UPDATE_INTERVAL = 30;
 var socket = io.connect('http://localhost:8000');
 var roomId = 'default';
 if (getQueryStrings()['room']) {
-  roomId = getQueryStrings()['room'];
+    roomId = getQueryStrings()['room'];
 }
 socket.emit('room', {room: roomId});
+
 socket.on('serverUpdate', function(data) {
-  // update player
+    console.log(data);
 })
 
 function getQueryStrings() {
-  var assoc  = {};
-  var decode = function (s) { return decodeURIComponent(s.replace(/\+/g, " ")); };
-  var queryString = location.search.substring(1);
-  var keyValues = queryString.split('&');
+    var assoc  = {};
+    var decode = function (s) { return decodeURIComponent(s.replace(/\+/g, " ")); };
+    var queryString = location.search.substring(1);
+    var keyValues = queryString.split('&');
 
-  for(var i in keyValues) {
-    var key = keyValues[i].split('=');
-    if (key.length > 1) {
-      assoc[decode(key[0])] = decode(key[1]);
+    for(var i in keyValues) {
+        var key = keyValues[i].split('=');
+        if (key.length > 1) {
+            assoc[decode(key[0])] = decode(key[1]);
+        }
     }
-  }
 
-  return assoc;
+    return assoc;
 }
 
 setInterval(function() {
