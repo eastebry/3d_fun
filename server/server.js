e2dec76d8b799b699183a3aca883027886d61c63
Edwin Zhang <yaz@yelp.com>
2015-07-30 10:43:51 -0700

    initil server commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var app = require('express').createServer();
+var io = require('socket.io')(app);
+
+app.listen(80);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
