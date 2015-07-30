8b9ddd18e766b66f1bc3760ae7a0d33f01e32d8c
Edwin Zhang <yaz@yelp.com>
2015-07-30 11:55:58 -0700

    room implementation

--- scripts/socket.js
+++ scripts/socket.js
@@ -1,5 +1,29 @@
 var socket = io.connect('http://localhost:8000');
-socket.on('news', function (data) {
-  console.log(data);
-  socket.emit('my other event', { my: 'data' });
-});
+var roomId = 'default';
+if (getQueryStrings()['room']) {
+  roomId = getQueryStrings()['room'];
+}
+socket.emit('room', {room: roomId});
+socket.on('serverUpdate', function(data) {
+  // update player
+})
+
+function getQueryStrings() {
+  var assoc  = {};
+  var decode = function (s) { return decodeURIComponent(s.replace(/\+/g, " ")); };
+  var queryString = location.search.substring(1);
+  var keyValues = queryString.split('&');
+
+  for(var i in keyValues) {
+    var key = keyValues[i].split('=');
+    if (key.length > 1) {
+      assoc[decode(key[0])] = decode(key[1]);
+    }
+  }
+
+  return assoc;
+}
+// socket.on('news', function (data) {
+//   console.log(data);
+//   socket.emit('my other event', { my: 'data' });
+// });
--- server/server.js
+++ server/server.js
@@ -8,10 +8,20 @@ var io = require('socket.io')(httpServer);
 server.use(express.static(__dirname + '/../'));
 httpServer.listen(8000);
 
+var socketRoomMap = {};
+var roomList = [];
+
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+  // socket.emit('news', { hello: 'world' });
+  socket.on('room', function (data) {
+    socket.join(data['room']);
+    socketRoomMap[socket.id] = data['room'];
+    // if new room
+    // setInterval
+  });
+
+  socket.on('clientUpdate', function (data) {
+    socket.broadcast.to(socketRoomMap[socket.id]).emit('serverUpdate', data);
   });
 });
 // serve
