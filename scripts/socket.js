52505c6d8789218606bc623181272bb45ccc2815
Andre Haas <andre@andrehaas.com>
2015-07-30 11:53:36 -0700

    Add some networking

--- scripts/socket.js
+++ scripts/socket.js
@@ -1,5 +1,23 @@
+CLIENT_UPDATE_INTERVAL = 30;
+
 var socket = io.connect('http://localhost:8000');
 socket.on('news', function (data) {
   console.log(data);
   socket.emit('my other event', { my: 'data' });
 });
+
+setInterval(function() {
+    var camera = localPlayer.camera;
+    socket.emit('clientUpdate', {
+        position: [
+            camera.position.x,
+            camera.position.y,
+            camera.position.z,
+        ],
+        rotation: [
+            camera.rotation.x,
+            camera.rotation.y,
+            camera.rotation.z,
+        ],
+    })
+}, CLIENT_UPDATE_INTERVAL);
--- server/server.js
+++ server/server.js
@@ -1,28 +1,26 @@
-
-// var server = require('http').Server(app);
 var express = require('express');
 var server = express();
 var httpServer = require('http').Server(server);
 var io = require('socket.io')(httpServer);
 
+SERVER_UPDATE_INTERVAL = 50;
+
+var clientSockets = [];
+var gameState = {};
+
 server.use(express.static(__dirname + '/../'));
 httpServer.listen(8000);
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+    socket.emit('news', { hello: 'world' });
+    socket.on('init', function (data) {
+        console.log(data);
+    });
+    socket.on('clientUpdate', function(data) {
+        gameState[socket.id] = data
+    });
 });
-// serve
-//
-// app.get('/', function (req, res) {
-//   res.sendfile('../index.html');
-// });
-//
-// io.on('connection', function (socket) {
-//   socket.emit('news', { hello: 'world' });
-//   socket.on('my other event', function (data) {
-//     console.log(data);
-//   });
-// });
+
+setInterval(function() {
+    
+}, SERVER_UPDATE_INTERVAL);
