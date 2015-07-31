3b624d08c4b69a1c1ace2011e90a65b54e2323c8
Jimmy Tang <jimmyt@yelp.com>
2015-07-31 13:49:27 -0700

    add textblock

---
+++ scripts/textblock.js
@@ -0,0 +1,17 @@
+function TextBlock(scene, text, pos) {
+    this.scene = scene;
+    this.mesh = BABYLON.Mesh.CreatePlane('box', 1, scene);
+    this.mesh.position.copyFrom(pos);
+    this.mesh.billboardMode = BABYLON.Mesh.BILLBOARDMODE_ALL;
+    this.setText(text);
+}
+TextBlock.prototype.setText = function(str) {
+    var mat = new BABYLON.StandardMaterial("outputplane", this.scene);
+    var tex = new BABYLON.DynamicTexture("dynamic texture", 512, this.scene, true);
+    var context2D = tex.getContext();
+    tex.hasAlpha = true;
+    tex.drawText(str, 0, 140, '72px Calibri', 'white');
+    mat.emissiveTexture = tex;
+    mat.diffuseTexture = tex;
+    this.mesh.material = mat;
+}
